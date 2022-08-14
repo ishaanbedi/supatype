c304bb440095b8019a0be962370ee55f4db9f820
Ishaan Bedi <bediishaan6@gmail.com>
2022-08-14 16:00:43 +0530

    Updated UI for user profile dashboard.

--- components/Account.js
+++ components/Account.js
@@ -1,6 +1,8 @@
+import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { supabase } from '../utils/supabaseClient'
 import ProfileStatsCard from './ProfileStatsCard'
+
 var userNameLocal = ""
 export default function Account({ session }) {
     const [loading, setLoading] = useState(true)
@@ -46,7 +48,7 @@ export default function Account({ session }) {
                 updated_at: new Date(),
             }
             let { error } = await supabase.from('profiles').upsert(updates, {
-                returning: 'minimal', // Don't return the value after inserting
+                returning: 'minimal'
             })
 
             if (error) {
@@ -65,7 +67,9 @@ export default function Account({ session }) {
                 <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-lg mx-auto text-center">
                         <h1 className='text-3xl font-bold sm:text-4xl my-4 text-[#D6D5A8] '>SupaProfile</h1>
-                        <h2 className="text-2xl sm:text-3xl">@{userNameLocal}</h2>
+                        {userNameLocal !== '' ? <h2 className="text-2xl sm:text-3xl bg-[#51557E] py-8 rounded-md">@{userNameLocal}</h2> : 'Register a username from below!'}
+
+
                     </div>
                     <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
                         <ProfileStatsCard
@@ -83,13 +87,14 @@ export default function Account({ session }) {
                     </div>
                     <div className="mt-12 text-center">
                         <div className="form-widget">
+                            <h1 className='text-2xl font-bold mt-8'>Edit details</h1>
                             <div className="relative my-4">
-                                <label className="block text-xs font-medium text-[#D6D5A8]" htmlFor="email"> Email </label>
+                                <label className="block text-xs font-medium text-[#D6D5A8]" htmlFor="email"> Email (if you want to change your email, please let us know at hi@ishn.xyz) </label>
                                 <input className="w-full p-3 mt-1 text-sm border-2 border-gray-200 rounded  cursor-not-allowed text-black" value={session.user.email} id="email" type="email" />
                             </div>
                             <div className="relative my-4">
                                 <label className="block text-xs font-medium text-[#D6D5A8]" htmlFor="username"> Display Name </label>
-                                <input className="w-full p-3 mt-1 text-sm border-2 border-gray-200 rounded text-black" id="email" type="email" value={username || ''} onChange={(e) => setUsername(e.target.value)} />
+                                <input className="w-full p-3 mt-1 text-sm border-2 border-gray-200 rounded text-black" id="email" type="email" defaultValue={username || ''} onChange={(e) => setUsername(e.target.value)} />
                             </div>
                             <div className='my-4 space-x-4'>
                                 <button
@@ -103,11 +108,11 @@ export default function Account({ session }) {
                                     </span>
 
                                     <span className="text-sm font-medium transition-all group-hover:mr-4">
-                                        {loading ? 'Loading ...' : 'Update Display Name'}
+                                        {loading ? 'Loading ...' : 'Update'}
                                     </span>
                                 </button>
                                 <button
-                                    className="relative inline-flex items-center px-8 py-3 overflow-hidden text-[#816797] border border-current rounded group active:text-indigo-500 focus:outline-none focus:ring" href="/download"
+                                    className="relative inline-flex items-center px-8 py-3 mt-2 overflow-hidden text-[#816797] border border-current rounded group active:text-indigo-500 focus:outline-none focus:ring" href="/download"
                                     onClick={() => supabase.auth.signOut()}
                                 >
                                     <span className="absolute right-0 transition-transform translate-x-full group-hover:-translate-x-4">
@@ -119,6 +124,21 @@ export default function Account({ session }) {
                                         Sign Out
                                     </span>
                                 </button>
+                                <Link href='/'>
+                                    <button
+                                        className="relative inline-flex items-center px-8 py-3 mt-2 overflow-hidden text-[#816797] border border-current rounded group active:text-indigo-500 focus:outline-none focus:ring" href="/download"
+                                    >
+                                        <span className="absolute right-0 transition-transform translate-x-full group-hover:-translate-x-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                                            </svg>
+                                        </span>
+
+                                        <span className="text-sm font-medium transition-all group-hover:mr-4">
+                                            Home
+                                        </span>
+                                    </button>
+                                </Link>
                             </div>
                         </div>
                     </div>
--- components/ProfileStatsCard.js
+++ components/ProfileStatsCard.js
@@ -4,7 +4,7 @@ function ProfileStatsCard(props) {
     return (
         <>
             <a
-                className="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-[#D6D5A8] hover:border-[#D6D5A8]"
+                className="block p-8 transition border bg-[#25303e] border-gray-800 shadow-xl rounded-xl hover:shadow-[#D6D5A8] hover:border-[#D6D5A8]"
             >
                 <h3 className="mt-4 text-xl font-bold text-white">{props.title}</h3>
                 <p className="mt-1 text-sm text-gray-300">

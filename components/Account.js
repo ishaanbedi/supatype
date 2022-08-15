d2f15ca44fed84dec437ea156c689c9d88d9b734
Ishaan Bedi <bediishaan6@gmail.com>
2022-08-15 19:44:28 +0530

    Fixed the minor bug for fall back to homepage after signing out

--- components/Account.js
+++ components/Account.js
@@ -120,20 +120,21 @@ export default function Account({ session }) {
                                         {loading ? 'Loading ...' : 'Update'}
                                     </span>
                                 </button>
-
-                                <button
-                                    className="relative inline-flex items-center px-8 py-3 mt-2 overflow-hidden text-white border border-current rounded group active:text-indigo-500 focus:outline-none focus:ring" href="/download"
-                                    onClick={() => supabase.auth.signOut()}
-                                >
-                                    <span className="absolute right-0 transition-transform translate-x-full group-hover:-translate-x-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                                        </svg>
-                                    </span>
-                                    <span className="text-sm font-medium transition-all group-hover:mr-4">
-                                        Sign Out
-                                    </span>
-                                </button>
+                                <Link href='/'>
+                                    <button
+                                        className="relative inline-flex items-center px-8 py-3 mt-2 overflow-hidden text-white border border-current rounded group active:text-indigo-500 focus:outline-none focus:ring" href="/download"
+                                        onClick={() => supabase.auth.signOut()}
+                                    >
+                                        <span className="absolute right-0 transition-transform translate-x-full group-hover:-translate-x-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                                            </svg>
+                                        </span>
+                                        <span className="text-sm font-medium transition-all group-hover:mr-4">
+                                            Sign Out
+                                        </span>
+                                    </button>
+                                </Link>
                                 <Link href='/'>
                                     <button
                                         className="relative inline-flex items-center px-8 py-3 mt-2 overflow-hidden text-white border border-current rounded group active:text-indigo-500 focus:outline-none focus:ring" href="/download"
